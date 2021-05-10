872ddaeeb03ed302db79fc25415a3507fb94bc08
Leonardo Maia Pugliese <leo.pug@hotmail.com>
2021-05-10 08:13:37 -0300

    card rotation very cool

--- PanGestureStudy/ViewController.swift
+++ PanGestureStudy/ViewController.swift
@@ -11,24 +11,50 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tealCardView = UIView()
-        tealCardView.backgroundColor = .systemTeal
-        tealCardView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
-        
-        view.addSubview(tealCardView)
-        
-        tealCardView.center = view.center
-        
-        tealCardView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleCardTapped)))
-        
+
+        addCards()
     }
+}
 
+extension ViewController {
+    private func addCards() {
+        
+        for cardNumber in 0...3 {
+            let tealCardView = UIView()
+            tealCardView.backgroundColor = cardNumber == 2 ? .systemTeal : .systemRed
+            tealCardView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+            
+            view.addSubview(tealCardView)
+            
+            tealCardView.center = view.center
+            
+            tealCardView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleCardTapped)))
+        }
+        
 
+    }
 }
 
 extension ViewController {
     @objc private func handleCardTapped(_ gesture: UIPanGestureRecognizer) {
-        print("lalal")
+        if let cardView = gesture.view {
+            let point = gesture.translation(in: view)
+
+            cardView.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+            
+            let rotationAngle = point.x / view.bounds.width
+            
+            print(rotationAngle,point.x,view.bounds.width)
+            
+            cardView.transform = CGAffineTransform(rotationAngle: rotationAngle)
+            
+            if gesture.state == .ended {
+                UIView.animate(withDuration: 0.3) {
+                    cardView.center = self.view.center
+                    cardView.transform = .identity
+                }
+            }
+        }
     }
 }
 
